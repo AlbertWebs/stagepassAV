73e243231e1c63b75e0ddd6501c2619bcf1b4d04
AlbertWebs <albertmuhatia@gmail.com>
2022-02-15 11:21:42 +0300

    Single Pages

--- resources/views/front/work/master.blade.php
+++ resources/views/front/work/master.blade.php
@@ -29,6 +29,12 @@
          rel="stylesheet"
          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"
          />
+
+         <!-- Latest compiled and minified CSS -->
+         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
       <!--Title of the site-->
       <title>
          StagePass Audio Visual Limited - The Best Audio visual company in Kenya,
